931be3eeb69f4080941a48a1741b77000dd8a7a2
Francesco Panciroli <fpanciroli@gmail.com>
2021-12-15 19:35:36 +0100

    feat: day 12

---
+++ src/day-12/day-12-a.js
@@ -0,0 +1,100 @@
+let input = [
+  'start-YA',
+  'ps-yq',
+  'zt-mu',
+  'JS-yi',
+  'yq-VJ',
+  'QT-ps',
+  'start-yq',
+  'YA-yi',
+  'start-nf',
+  'nf-YA',
+  'nf-JS',
+  'JS-ez',
+  'yq-JS',
+  'ps-JS',
+  'ps-yi',
+  'yq-nf',
+  'QT-yi',
+  'end-QT',
+  'nf-yi',
+  'zt-QT',
+  'end-ez',
+  'yq-YA',
+  'end-JS',
+]
+
+// input = ['start-A', 'start-b', 'A-c', 'A-b', 'b-d', 'A-end', 'b-end']
+// input = [
+//   'dc-end',
+//   'HN-start',
+//   'start-kj',
+//   'dc-start',
+//   'dc-HN',
+//   'LN-dc',
+//   'HN-end',
+//   'kj-sa',
+//   'kj-HN',
+//   'kj-dc',
+// ]
+// input = [
+//   'fs-end',
+//   'he-DX',
+//   'fs-he',
+//   'start-DX',
+//   'pj-DX',
+//   'end-zg',
+//   'zg-sl',
+//   'zg-pj',
+//   'pj-he',
+//   'RW-he',
+//   'fs-DX',
+//   'pj-RW',
+//   'zg-RW',
+//   'start-pj',
+//   'he-WI',
+//   'zg-he',
+//   'pj-fs',
+//   'start-RW',
+// ]
+
+const nodes = {
+  // start: {
+  //   name: 'start',
+  //   isBig: false,
+  //   links: [],
+  // },
+}
+
+const newNode = (name) => ({
+  name,
+  isBig: name === name.toUpperCase(),
+  links: [],
+})
+
+for (const link of input) {
+  const [from, to] = link.split('-')
+  nodes[from] = nodes[from] || newNode(from)
+  nodes[to] = nodes[to] || newNode(to)
+  nodes[from].links.push(nodes[to])
+  nodes[to].links.push(nodes[from])
+}
+
+// console.log(nodes)
+const paths = []
+const move = (from, currentPath) => {
+  for (const link of from.links) {
+    const newPath = [...currentPath, link.name]
+    if (link.name === 'end') {
+      paths.push(newPath)
+    } else {
+      if (link.isBig || !currentPath.includes(link.name)) {
+        move(link, newPath)
+      }
+    }
+  }
+}
+
+move(nodes.start, ['start'])
+// console.log(paths)
+console.log(paths.length)
