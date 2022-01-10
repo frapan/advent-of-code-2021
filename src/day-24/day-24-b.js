553ce6e8f0eef72d75334b0baba419995c3d616e
Francesco Panciroli <fpanciroli@gmail.com>
2022-01-10 11:51:05 +0100

    feat: day 24 part b

--- src/day-24/day-24-b.js
+++ src/day-24/day-24-b.js
@@ -15,18 +15,18 @@ const getValues = (input) => {
 const values = getValues(input)
 
 const code = []
-code[4] = Math.min(9, 9 + values[3].y + values[4].x)
-code[6] = Math.min(9, 9 + values[5].y + values[6].x)
-code[8] = Math.min(9, 9 + values[7].y + values[8].x)
-code[9] = Math.min(9, 9 + values[2].y + values[9].x)
-code[10] = Math.min(9, 9 + values[1].y + values[10].x)
-code[11] = Math.min(9, 9 + values[0].y + values[11].x)
-code[13] = Math.min(9, 9 + values[12].y + values[13].x)
+code[4] = Math.max(1, 1 + values[3].y + values[4].x)
+code[6] = Math.max(1, 1 + values[5].y + values[6].x)
+code[8] = Math.max(1, 1 + values[7].y + values[8].x)
+code[9] = Math.max(1, 1 + values[2].y + values[9].x)
+code[10] = Math.max(1, 1 + values[1].y + values[10].x)
+code[11] = Math.max(1, 1 + values[0].y + values[11].x)
+code[13] = Math.max(1, 1 + values[12].y + values[13].x)
+code[12] = code[13] - values[13].x - values[12].y
+code[7] = code[8] - values[8].x - values[7].y
+code[5] = code[6] - values[6].x - values[5].y
 code[3] = code[4] - values[4].x - values[3].y
 code[2] = code[9] - values[9].x - values[2].y
 code[1] = code[10] - values[10].x - values[1].y
 code[0] = code[11] - values[11].x - values[0].y
-code[5] = code[6] - values[6].x - values[5].y
-code[12] = code[13] - values[13].x - values[12].y
-code[7] = code[8] - values[8].x - values[7].y
 console.log(code.join(''))
